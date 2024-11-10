aa9169663b0e55b272b4c698d153d4d7be617467
Asma khemiri <benbrahimsalma18@gmail.com>
2024-11-10 18:40:53 +0100

    modification de app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import SignUp from './components/SignUp/SignUp';
+import Navbar from './components/Navbar/Navbar'; 
+import AOS from 'aos'; 
+import 'aos/dist/aos.css'; 
+import { Toaster } from 'react-hot-toast'; // Importez Toaster
+import './App.css'; 
+import Login from './components/Login/Login';
+import { auth } from './firebase/firebase';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+
+const App = () => {
+  const [orderPopup, setOrderPopup] = useState(false); 
+  const [userName,setUserName]=useState("")
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup); 
+  };
+
+  useEffect(() => {
+    AOS.init({ 
+      offset: 100,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+
+    });
+    AOS.refresh(); 
+  }, []);
+  useEffect(()=>
+  {
+    auth.onAuthStateChanged((user)=>{
+      if(user)
+      {
+       setUserName(user.displayName) 
+      }
+      else
+      {
+        setUserName("")
+      }
+    });
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar handleOrderPopup={handleOrderPopup}  /> {/* Affiche Navbar sur toutes les pages */}
+        <Routes>
+          <Route path="/" element={<Home handleOrderPopup={handleOrderPopup} />} />
+          <Route path="/login" element={<Login />} /> {/* Assurez-vous que c'est correct */}
+          <Route path="/signup" element={<SignUp />} /> {/* Route pour SignUp */}
+        </Routes>
+        <Toaster /> {/* Affichez le Toaster pour les notifications */}
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
